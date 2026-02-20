10f1537a5f8e6d8dcee6272ca54c24b2f20b11d4
Victor Silva do Valle <victor.silvadovalle@gmail.com>
2026-02-19 23:34:28 -0300

    Correção da parte anterior

--- src/app.js
+++ src/app.js
@@ -1,18 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-<<<<<<< HEAD
-
 const app = express();
 
-app.use = express();
-
-app.get("/", (req,res) => {res.send("API Protocolo funcionando")});
-
-app.listen(3000, () => {console.log("Servidor rodando na porta 3000")} );
-=======
-const app = express();
-
-app.use(cors()); // mudei aqui "app.use = express();, para ser app.use(cors());"
+app.use(cors());
 app.use(express.json());
 
 let protocolos = [];
@@ -40,4 +30,3 @@ app.post("/api/protocolos", (req, res) => {
 app.listen(3000, () => {
   console.log("Servidor rodando na porta 3000");
 });
->>>>>>> 22f39c8 (feat: interface de protocolos e integracao inicial)
